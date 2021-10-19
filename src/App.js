3b84fed99bd9d05bb7ed446c309b3b37a2f05977
Hz Fabian Aparicio <herzonfabian100@gmail.com>
2021-10-18 18:42:32 -0700

    Created embedded JS on JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+
 import * as React from 'react';
 
+
+function getTitle(title) {
+  return title;
+}
 function App() {
-  return (<div> <h1> Hello World </h1></div>
+  return (<div> <h1> Hello {getTitle('World')} </h1>
+    <label htmlFor="sarch">Search:</label>
+    <input id="search" type="text" />
+  </div>
 
   );
 }
