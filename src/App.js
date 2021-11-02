b2b79231f789e8892170fce80cb560689605d0b0
Hz Fabian Aparicio <herzonfabian100@gmail.com>
2021-11-01 22:18:24 -0700

    Created React Props

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-function List() {
+
+function List(props) {
   return (
+
     <ul>
-      {list.map(function (item) {
+      {props.list.map(function (item) {
         return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
+          <Item item={item} />
         );
       })}
 
@@ -39,17 +16,59 @@ function List() {
 
   );
 }
+const Item = (props) => {
+  const item = props.item;
+  return (
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{props.item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  );
+}
 
 function Search() {
+
+  const handleChange = (event) => {
+    console.log(event);
+  }
+  const handleMouseOver = (event) => {
+    console.log(event);
+  }
+
   return (
     <div>
       <label htmlFor="sarch">Search:</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} onMouseOver={handleMouseOver} />
     </div>
   );
 }
 
 function App() {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+
   return (
     <div>
       <h1> My Hacker Story</h1>
@@ -57,7 +76,8 @@ function App() {
       <Search />
 
       <hr />
-      <List />
+      <List list={stories} />
+
 
     </div >
 
